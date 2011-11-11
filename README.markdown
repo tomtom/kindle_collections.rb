5727f200645edd53576567126f16061c97eedd1f
Tom <micathom@gmail.com>
2011-11-11 14:41:04 +0100

    Initial

---
+++ README.markdown
@@ -0,0 +1,116 @@
+Introduction
+============
+
+IMPORTANT: Backup your original `collection.json` file before running 
+this script!
+
+This is a simple ruby script to automatically classify documents in 
+kindle collections based on the directory structure.
+
+Collections are defined on basis of the second directory name. Examples:
+
+<table>
+<th>
+<td>Filename</td>
+<td>Collection</td>
+</th>
+<tr>
+<td>`documents/filename.ext`</td>
+<td>Such a file won't be added to a collection</td>
+</tr>
+<tr>
+<td>`documents/collection/filename.ext`</td>
+<td>`collection`</td>
+</tr>
+<tr>
+<td>`documents/collection/subdir/filename.ext`</td>
+<td>`collection`</td>
+</tr>
+</table>
+
+For now, only Kindle version 2 (e.g. Kindle DX) is supported (i.e. 
+tested). I have no idea if the script works for other versions.
+
+Example usage:
+
+    kindle_collections.rb --dir /media/Kindle
+
+You have to hard reset the kindle for the changes to take effect.
+
+
+Installation
+============
+
+Copy or symlink `kindle_collections.rb` to a directory in $PATH.
+
+
+Configuration
+=============
+
+The script can be configured via YAML files that is searched in:
+
+- `/etc/kindle_collections.yml`
+- `$HOME/.kindle_collections.yml`
+- `$HOME/.kindle_collections_$HOSTNAME.yml`
+- `Windows: %USERPROFILE%/kindle_collections.yml`
+
+Each command-line option can be configured via this YAML file.
+
+Example configuration file:
+
+    --- 
+    dir: /media/Kindle
+    rx: !ruby/regexp /\.(azw|mobi|txt|pdf)$/
+    subdirs: 
+    - documents
+
+An initial configuration file can be created with the following command:
+
+    kindle_collections.rb --print-config > ~/.kindle_collections.yml
+
+
+Known Problems
+==============
+
+ATM certain files cannot be properly added to collections. This problem 
+affects filenames that include underscores ("\_"). In order to avoid 
+this problem, users can pass a template JSON file to 
+`kindle_collections.rb`:
+
+    kindle_collections.rb --json /media/Kindle/system/collections_base.json
+
+In order to get such a base JSON file, proceed as follows:
+
+1. Connect the kindle to your computer
+2. Run `kindle_collections.rb`
+3. Disconnect the kindle
+4. Hard-reset the kindle
+5. Add the missing documents to collections
+6. Connect the kindle to your computer
+7. `cd /media/Kindle/system`
+9. Run `kindle_collections.rb --print-diff collections.json > collections_base.json`
+
+
+Requirements
+============
+
+- Ruby 1.8
+- json gem
+
+
+Licence
+=======
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/
+
---
+++ kindle_collections.rb
@@ -0,0 +1,267 @@
+#!/usr/bin/env ruby
+# kindle_collections.rb -- Automatically manage kindle collections
+# @Author:      Tom Link (micathom AT gmail com)
+# @License:     GPL (see http://www.gnu.org/licenses/gpl.txt)
+# @Created:     2011-11-11.
+# @Last Change: 2011-11-11.
+# @Revision:    106
+
+# require ''
+
+require 'optparse'
+require 'fileutils'
+require 'rbconfig'
+require 'logger'
+require 'digest/sha1'
+if RUBY_VERSION =~ /^1\.8/
+    require 'rubygems'
+    require 'json/pure'
+else
+    require 'json'
+end
+
+class KindleCollections
+    APPNAME = 'kindle_collections.rb'
+    VERSION = '0.0'
+    HELP = {}
+    CONFIGS = []
+    if ENV['WINDIR']
+        CONFIGS << File.join(File.dirname(ENV['WINDIR'].gsub(/\\/, '/')) ,'kindle_collections.yml')
+    else
+        CONFIGS << '/etc/kindle_collections.yml'
+    end
+    if ENV['HOME']
+        CONFIGS << File.join(ENV['HOME'].gsub(/\\/, '/'), '.kindle_collections.yml')
+        if ENV['HOSTNAME']
+            CONFIGS << File.join(ENV['HOME'].gsub(/\\/, '/'), ".kindle_collections_#{ENV['HOSTNAME']}.yml")
+        end
+    elsif ENV['USERPROFILE']
+        CONFIGS << File.join(ENV['USERPROFILE'].gsub(/\\/, '/'), 'kindle_collections.yml')
+    end
+    CONFIGS.delete_if {|f| !File.exist?(f)}
+
+    class AppLog
+        def initialize(output=$stdout)
+            @output = output
+            $logger = Logger.new(output)
+            $logger.progname = defined?(APPNAME) ? APPNAME : File.basename($0, '.*')
+            $logger.datetime_format = "%H:%M:%S"
+            AppLog.set_level
+        end
+    
+        def self.set_level
+            if $DEBUG
+                $logger.level = Logger::DEBUG
+            elsif $VERBOSE
+                $logger.level = Logger::INFO
+            else
+                $logger.level = Logger::WARN
+            end
+        end
+    end
+
+
+    class << self
+    
+        def with_args(args)
+    
+            AppLog.new
+    
+            config = Hash.new
+            config['dir'] = Dir.pwd
+            config['subdirs'] = ['documents']
+            config['collections'] = {}
+            config['json'] = ''
+            config['rx'] = /\.(azw|mobi|txt|pdf)$/
+            config['kindle_version'] = 2
+            CONFIGS.each do |file|
+                begin
+                    config.merge!(YAML.load_file(file))
+                rescue TypeError
+                    $logger.error "Error when reading configuration file: #{file}"
+                end
+            end
+            opts = OptionParser.new do |opts|
+                opts.banner =  "Usage: #{File.basename($0)} [OPTIONS]<+CURSOR+>"
+                opts.separator ' '
+                opts.separator 'kindle_collections.rb is a free software with ABSOLUTELY NO WARRANTY under'
+                opts.separator 'the terms of the GNU General Public License version 2 or newer.'
+                opts.separator ' '
+
+                opts.separator 'General Options:'
+
+                opts.on('-d', '--dir DIR', String, 'Kindle base directory') do |value|
+                    config['dir'] = value
+                end
+
+                opts.on('-j', '--json FILE', String, 'Re-use an existing collections.json') do |value|
+                    config['json'] = value
+                end
+
+                opts.on('-k VERSION', '--kindle VERSION', Integer, 'Kindle version (default: 2)') do |value|
+                    config['kindle_version'] = value
+                end
+                
+                opts.on('-p', '--pattern REGEXP', String, 'Register only files matching this regular expression') do |value|
+                    config['rx'] = Regexp.new(value)
+                end
+
+                opts.on('--print-config', 'Print the configuration and exit') do |bool|
+                    # puts "Configuration files: #{CONFIGS}"
+                    puts YAML.dump(config)
+                    exit
+                end
+
+                opts.on('--print-diff JSON', String, 'Print entries in a JSON file that are not included in a current scan') do |value|
+                    config['print-diff'] = value
+                end
+                
+                opts.on('-s', '--subdir DIR', String, 'Add sub-directory') do |value|
+                    config['subdir'] << value
+                end
+
+                opts.separator ' '
+                opts.separator 'Other Options:'
+            
+                opts.on('--debug', 'Show debug messages') do |v|
+                    $DEBUG   = true
+                    $VERBOSE = true
+                    AppLog.set_level
+                end
+            
+                opts.on('-v', '--verbose', 'Run verbosely') do |v|
+                    $VERBOSE = true
+                    AppLog.set_level
+                end
+            
+                opts.on_tail('-h', '--help', 'Show this message') do
+                    puts opts
+                    exit 1
+                end
+            end
+            $logger.debug "command-line arguments: #{args}"
+            argv = opts.parse!(args)
+            $logger.debug "config: #{config}"
+            $logger.debug "argv: #{argv}"
+            if config['kindle_version'] != 2
+                $logger.fatal "Only kindle version 2 is supported"
+                exit 5
+            end
+
+            unless config['json'].nil? or config['json'].empty?
+                if File.exists?(config['json'])
+                    $logger.info "Read base JSON file: #{config['json']}"
+                    serialized = File.read(config['json'])
+                    begin
+                        config['collections'] = JSON.parse(serialized)
+                    rescue JSON::ParserError => e
+                        $logger.error "Error when parsing JSON file #{config['json'].inspect}: #{e}"
+                    end
+                else
+                    $logger.error "JSON file does not exist: #{config['json']}"
+                end
+            end
+    
+            return KindleCollections.new(config, argv)
+        end
+    
+    end
+
+    # config ... hash
+    # args   ... array of strings
+    def initialize(config, args)
+        @config = config
+        @args   = args
+        @files  = []
+        @collections = {}
+    end
+
+    def process
+        collect_files
+        classify_files
+        if @config.has_key?('print-diff')
+            print_diff
+        else
+            write_json
+        end
+    end
+
+    def collect_files
+        @files  = []
+        FileUtils.cd(@config['dir']) do
+            for subdir in @config['subdirs']
+                $logger.debug "Scan subdir: #{subdir}"
+                if File.directory?(subdir)
+                    pattern = File.join(subdir, '**', '*')
+                    $logger.debug "Use pattern: #{pattern}"
+                    @files += Dir[pattern]
+                end
+            end
+        end
+        @files.delete_if {|filename| filename !~ @config['rx']}
+        $logger.debug @files.inspect
+    end
+
+    def classify_files
+        @collections = {}.merge(@config['collections'])
+        @files.each do |filename|
+            parts = filename.split(/[\\\/]/)
+            if parts.count > 2
+                collection = "#{parts[1]}@en-US"
+                @collections[collection] ||= {'items' => []}
+                id = if filename =~ /^.*?-asin_([a-zA-Z0-9_]+)-type_([a-zA-Z0-9_]+)-v_([0-9]+)\.([^[:space:].]+)$/
+                         "##{$1}^#{$2}"
+                     else
+                         "*#{Digest::SHA1.hexdigest("/mnt/us/#{filename}")}"
+                     end
+                $logger.debug "#{filename} :: #{collection} << #{id}"
+                @collections[collection]['items'] << id
+            end
+        end
+        $logger.info "Collections: #{@collections.count}"
+    end
+
+    def write_json
+        serialized = @collections.to_json
+        $logger.debug "JSON: #{serialized}"
+        collections_json = File.join(@config['dir'], 'system', 'collections.json')
+        if File.exists?(collections_json)
+            collections_bak = File.join(@config['dir'], 'system', "collections_bak_#{Time.now.strftime("%Y-%m-%d")}.json")
+            $logger.info "Backup collections.json to #{collections_bak}"
+            File.rename(collections_json, collections_bak)
+        end
+        $logger.warn "Write #{collections_json}"
+        File.open(collections_json, "w") {|f| f.print serialized}
+        $logger.warn "You have to hard reset the kindle for the changes to take effect!"
+    end
+
+    def print_diff
+        if File.exists?(@config['print-diff'])
+            serialized = File.read(@config['print-diff'])
+            other = JSON.parse(serialized)
+            diff = {}
+            for collection, vals in other
+                if @collections.has_key?(collection)
+                    collected_items = @collections[collection]['items']
+                    for item in vals['items']
+                        unless collected_items.include?(item)
+                            diff[collection] ||= {'items' => []}
+                            diff[collection]['items'] << item
+                        end
+                    end
+                else
+                    diff[collection] = vals
+                end
+            end
+            puts diff.to_json
+        else
+            $logger.error "JSON file does not exist: #{@config['print-diff']}"
+        end
+    end
+end
+
+
+if __FILE__ == $0
+    KindleCollections.with_args(ARGV).process
+end
+
